c2db6ef449a714ec94f5ad5af69982c8255392cf
Lylindsay <lindsay_lemire@hotmail.com>
2022-10-18 21:54:27 -0400

    debut liste-forfaits

---
+++ src/app/forfait.ts
@@ -0,0 +1,16 @@
+export interface Forfait {
+    id?: number;
+    nom: string,
+    description: string,
+    code: string,
+    catégories: string,
+    etablissement: {
+        id?: number;
+        nom: string,
+        description: string;
+    },
+    datedebut: string,
+    datefin: string,
+    prix: number,
+    avis: string;
+}
---
+++ src/app/mock-forfaits.ts
@@ -0,0 +1,74 @@
+import { Forfait } from "./forfait";
+
+export const FORFAITS: Forfait[] = [
+    {
+        nom:'Nom du forfait 1',
+        description: 'Description du forfait 1',
+        code: 'ABC000',
+        catégories: 'Hebergement',
+        etablissement: {
+            nom: 'nom de letablissement 1',
+            description: 'description 1'
+        },
+        datedebut: '01-01-23',
+        datefin: '31-12-23',
+        prix: 10,
+        avis: '1'
+    },
+    {
+        nom:'Nom du forfait 2',
+        description: 'Description du forfait 2',
+        code: 'ABC000',
+        catégories: 'Hebergement',
+        etablissement: {
+            nom: 'nom de letablissement 2',
+            description: 'description 2'
+        },
+        datedebut: '01-01-23',
+        datefin: '31-12-23',
+        prix: 20,
+        avis: '2'
+    },
+    {
+        nom:'Nom du forfait 3',
+        description: 'Description du forfait 3',
+        code: 'ABC000',
+        catégories: 'Hebergement',
+        etablissement: {
+            nom: 'nom de letablissement 3',
+            description: 'description 3'
+        },
+        datedebut: '01-01-23',
+        datefin: '31-12-23',
+        prix: 30,
+        avis: '3'
+    },
+    {
+        nom:'Nom du forfait 4',
+        description: 'Description du forfait 4',
+        code: 'ABC000',
+        catégories: 'Hebergement',
+        etablissement: {
+            nom: 'nom de letablissement 4',
+            description: 'description 4'
+        },
+        datedebut: '01-01-23',
+        datefin: '31-12-23',
+        prix: 40,
+        avis: '4'
+    },
+    {
+        nom:'Nom du forfait 5',
+        description: 'Description du forfait 5',
+        code: 'ABC000',
+        catégories: 'Hebergement',
+        etablissement: {
+            nom: 'nom de letablissement 5',
+            description: 'description 5'
+        },
+        datedebut: '01-01-23',
+        datefin: '31-12-23',
+        prix: 50,
+        avis: '5'
+    },
+]
